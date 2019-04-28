ad70f5c57d89a5da01f4856b5ab512aa79b4456a
vinnyt123 <vinny.tunnell@gmail.com>
2019-04-28 14:45:43 +1200

    Set up menu background in mainmenustate instead of main menu fxml

--- src/model/MainMenuState.java
+++ src/model/MainMenuState.java
@@ -2,13 +2,17 @@ package model;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 import java.util.HashMap;
 
-public class MainMenuState extends Pane {
+public class MainMenuState extends StackPane {
 
+    private static final String BACKGROUND_IMAGE_PATH = "images/MenuBackground.png";
     public static String mainMenuID = "Main Menu";
     public static String mainMenuFile = "view/MainMenu.fxml";
     public static String GameOverID = "Game Complete";
@@ -17,12 +21,37 @@ public class MainMenuState extends Pane {
     private FXMLLoader loader;
 
     private HashMap<String, Node> loadedScreens = new HashMap<>();
+    private Pane background;
+    private Pane controlsLayer = new Pane();
 
 
     public MainMenuState() {
+        this.setPrefWidth(Main.SCENE_WIDTH);
+        this.setPrefHeight(Main.SCENE_HEIGHT);
+
+        createBackground();
+        this.getChildren().addAll(background, controlsLayer);
         loadScreen(mainMenuID, mainMenuFile);
         loadScreen(GameOverID, GameOverFile);
         setScreen(mainMenuID);
+        System.out.println(background.getBoundsInParent());
+        System.out.println(controlsLayer.getBoundsInParent());
+    }
+
+    private void createBackground() {
+        ImageView backgroundImage = new ImageView(new Image(BACKGROUND_IMAGE_PATH));
+        backgroundImage.setFitWidth(Main.SCENE_WIDTH);
+        backgroundImage.setFitHeight(Main.SCENE_HEIGHT);
+       // backgroundImage.setPreserveRatio(true);
+        //backgroundImage.setPickOnBounds(true);
+        background = new Pane();
+        background.setPrefWidth(Main.SCENE_WIDTH);
+        background.setPrefHeight(Main.SCENE_HEIGHT);
+        background.setMinHeight(Main.SCENE_HEIGHT);
+        background.setMinWidth(Main.SCENE_WIDTH);
+        background.setMaxWidth(Main.SCENE_WIDTH);
+        background.setMaxHeight(Main.SCENE_HEIGHT);
+        background.getChildren().add(backgroundImage);
     }
 
     public void loadScreen(String screenName, String fxmlPath) {
@@ -37,9 +66,9 @@ public class MainMenuState extends Pane {
     public void setScreen(String name) {
         if (loadedScreens.containsKey(name)) {
             if (!this.getChildren().isEmpty()) {
-                this.getChildren().clear();
+                controlsLayer.getChildren().clear();
             }
-            this.getChildren().add(loadedScreens.get(name));
+            controlsLayer.getChildren().add(loadedScreens.get(name));
         } else {
             System.out.println("Screen " + name + " has not been loaded.");
         }
