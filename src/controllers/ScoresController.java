2708be6f47c06b9bab5f929947285b1151d56e0b
vinnyt123 <vinny.tunnell@gmail.com>
2019-04-29 12:39:12 +1200

    Fixed spikes and changes table records empty message

--- src/controllers/ScoresController.java
+++ src/controllers/ScoresController.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -39,6 +40,7 @@ public class ScoresController {
     @FXML
     public void initialize() {
         tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tableView.setPlaceholder(new Label("No High Scores Saved"));
     }
 
     @FXML
